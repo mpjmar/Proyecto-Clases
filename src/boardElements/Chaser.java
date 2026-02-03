112649b5507a7aa573b70173087c27e0a24d91a5
M. Paz Jimenez <meripeich@gmail.com>
2026-02-03 11:58:37 +0100

    cambios Juan Antonio

---
+++ src/boardElements/Chaser.java
@@ -0,0 +1,5 @@
+package boardElements;
+
+public class Chaser {
+    
+}
---
+++ src/game/Game.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Game {
+    
+    public Game() {
+
+    }
+
+    public void launch() throws InterruptedException {
+        do { 
+            Thread.sleep(100);
+        } while (true);
+    }
+}
